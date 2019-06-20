fc6009d726129e2b2111127a30be20ade373a165
gafaronline <gafaronline@hotmail.com>
2019-06-20 17:50:55 +0100

    filter on country

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -1,7 +1,7 @@
 const request = require('request')
 
 const geocode = (address, callback) => {
-    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/'+ encodeURIComponent(address) + '.json?proximity=-74.70850,40.78375&access_token=pk.eyJ1IjoiZ2FmYXJvbmxpbmUiLCJhIjoiY2p3cDBkM2hkMm4wbTN6bzJxb2hmczM3ciJ9.tMrZoSKGOslW8TzJhNM7BA'
+    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/'+ encodeURIComponent(address) + '.json?proximity=-74.70850,40.78375&access_token=pk.eyJ1IjoiZ2FmYXJvbmxpbmUiLCJhIjoiY2p3cDBkM2hkMm4wbTN6bzJxb2hmczM3ciJ9.tMrZoSKGOslW8TzJhNM7BA&country=NG'
 
     request({url: url, json: true}, (error, {body} ) => {
         if(error) {
